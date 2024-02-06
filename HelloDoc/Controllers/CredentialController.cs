89a3a05be729a37686687c908810286b09801014
yashpatel-tatva <yashb.patel@etatvasoft.com>
2024-02-06 22:23:14 +0530

    ...

---
+++ HelloDoc/Controllers/CredentialController.cs
@@ -0,0 +1,25 @@
+﻿using HelloDoc.DataContext;
+using HelloDoc.DataModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelloDoc.Controllers
+{
+    public class CredentialController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CredentialController(ApplicationDbContext context) { 
+            _context = context;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(Region region)
+        {
+            var correct = await _context.Regions
+                .FirstOrDefaultAsync(m => m.Name == region.Name);
+            if(correct.Abbreviation == region.Abbreviation) {
+                return View("Correct");
+            }
+            return View("hello");
+        }
+    }
+}
