81cce5653f93c9932a3bdf7af817f5d9159572a9
YashPatel117 <yeshpatel0703@gmail.com>
2024-04-07 09:30:00 +0530

    emailviewmodel

---
+++ DataModels/AdminSideViewModels/EmailSMSLogsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace DataModels.AdminSideViewModels
+{
+	public class EmailSMSLogsViewModel
+	{
+		public int id { get; set;}
+		public string Recipient { get; set;	 }
+		public string Action { get; set; }
+		public string RoleName { get; set; }
+		public string Mobile { get; set; }
+		public string Email { get; set; }
+		public DateTime CreatedDate { get; set; }
+		public DateTime SentDate { get; set; }
+		public bool Sent { get; set; }
+		public int Senttries { get; set; }
+		public string ConfirmationNo { get; set; }
+	}
+}
+
